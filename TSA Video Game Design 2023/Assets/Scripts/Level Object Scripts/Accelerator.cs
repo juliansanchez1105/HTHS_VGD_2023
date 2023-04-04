7e338308c082f39301045370115b0ed6fc2f1dc7
ZacLevy <zalevy@ctemc.org>
2023-04-04 09:51:39 -0400

    Some more small changes
    Added accelerator (temp sprite)
    added sprite to gravity swapper
    I think thats all

--- TSA Video Game Design 2023/Assets/Scripts/Level Object Scripts/Accelerator.cs
+++ TSA Video Game Design 2023/Assets/Scripts/Level Object Scripts/Accelerator.cs
@@ -6,19 +6,21 @@ public class Accelerator : MonoBehaviour
 {
     private Vector2 direction;
     [SerializeField] private float exitSpeed;
+    [SerializeField] private Rigidbody2D ballBody;
+    private Vector2 preVelocity;
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        
+        preVelocity = ballBody.velocity;
     }
 
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.gameObject.tag == "Player"){
-            collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
-            collider.gameObject.GetComponent<Transform>().position = transform.position;
-            collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(transform.up.x * exitSpeed, transform.up.y * exitSpeed, 0);
+            //collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+            //collider.gameObject.GetComponent<Transform>().position = transform.position;
+            collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(preVelocity.x * exitSpeed, preVelocity.y * exitSpeed, 0);
         }
     }
 }
---
+++ TSA Video Game Design 2023/Assets/Scripts/Level Object Scripts/Launcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    private Vector2 direction;
+    [SerializeField] private float exitSpeed;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == "Player"){
+            collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+            collider.gameObject.GetComponent<Transform>().position = transform.position;
+            collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(transform.up.x * exitSpeed, transform.up.y * exitSpeed, 0);
+        }
+    }
+}
