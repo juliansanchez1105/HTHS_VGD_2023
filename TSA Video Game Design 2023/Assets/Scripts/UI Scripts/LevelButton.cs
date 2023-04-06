6a0aa4568ace290fdb1b309dcd3899a0c8ef66d6
juliansanchez1105 <75316436+juliansanchez1105@users.noreply.github.com>
2023-04-05 22:56:11 -0400

    Added card functionality

--- TSA Video Game Design 2023/Assets/Scripts/UI Scripts/LevelButton.cs
+++ TSA Video Game Design 2023/Assets/Scripts/UI Scripts/LevelButton.cs
@@ -7,7 +7,11 @@ public class LevelButton : MonoBehaviour
 {
     [SerializeField] private GameObject card;
     [SerializeField] private Sprite accessed;
+    [SerializeField] private int levelNum;
     public void CardCollected(){
         card.GetComponent<Image>().sprite = accessed;
+    }
+    public int LevelNum{
+        get{return levelNum;}
     }
 }
